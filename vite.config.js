fc8a73e03afd7c55e8e1ad91d2196762de9804e0
micha-dev <astalejo6@gmail.com>
2024-10-29 19:27:40 -0500

    README.md Isabel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    enviroment: 'jsdom',
+    stupFiles: './src/setupTest'
+  }
 })
